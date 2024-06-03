44c2a95f1a7eeb67247509eff6eb163765849022
stefanysch <stefany@edu.unifil.br>
2024-06-03 16:30:42 -0300

    resultado-escolar

--- introducao-a-java/src/ResultadoEscolar.java
+++ introducao-a-java/src/ResultadoEscolar.java
@@ -1,11 +1,11 @@
 public class ResultadoEscolar {
   
   public static void main(String[] args) {
-    int nota= 8;
+    int nota = 8;
 
-    if(nota >= 7)
+    if (nota >= 7)
       System.out.println("Aprovado!");
-    else if (nota >= 5 && <7)
+    else if (nota >= 5 && nota < 7)
       System.out.println("Prova de Recuperação!");
     else
       System.out.println("Reprovado!");
