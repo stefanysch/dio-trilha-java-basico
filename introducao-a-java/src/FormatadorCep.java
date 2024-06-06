0f5dad0253b775d31b481cfb62741003f1b59116
stefanysch <stefany@edu.unifil.br>
2024-06-06 16:08:31 -0300

    .

---
+++ introducao-a-java/src/FormatadorCep.java
@@ -0,0 +1,16 @@
+public class FormatadorCep {
+  public static void main(String[] args) {
+    try {
+      String cepFormatado = formatarCep ("23765064");
+      System.out.println(cepFormatado);
+    } catch (CepInvalidoException e) {
+      System.out.println("O cep não corresponde com as regras!");;
+    }
+  }
+  static String formatarCep(String cep) throws CepInvalidoException {
+    if(cep.length() != 8)
+      throw new CepInvalidoException();
+
+      return "23.765-064";
+  }
+}
