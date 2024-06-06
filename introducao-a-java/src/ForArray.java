4eb17689f776dc1148a3ddfe2d387b21c06d09d5
stefanysch <stefany@edu.unifil.br>
2024-06-06 12:00:10 -0300

    .

---
+++ introducao-a-java/src/ForArray.java
@@ -0,0 +1,9 @@
+public class ForArray {
+  public static void main(String[] args) {
+    String alunos [] = {"Felipe", "Jonas", "Laura", "Marcos"};
+
+    for (int x = 0; x < alunos.length; x++) {
+      System.out.println("O aluno no índice x= " + x + " é: " + alunos[x]);
+    }
+  }
+}
