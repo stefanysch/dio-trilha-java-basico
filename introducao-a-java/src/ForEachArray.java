a8484953ba6e6eaea0f02d1e9b49b0e9703a310d
stefanysch <stefany@edu.unifil.br>
2024-06-06 12:05:35 -0300

    .

---
+++ introducao-a-java/src/ForEachArray.java
@@ -0,0 +1,9 @@
+public class ForEachArray {
+public static void main(String[] args) {
+    String alunos [] = {"Felipe", "Jonas", "Laura", "Marcos"};
+
+    for(String aluno : alunos) {
+      System.out.println("Nome do aluno é: " + aluno);
+    } 
+  }
+}
